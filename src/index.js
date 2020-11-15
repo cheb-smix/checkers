1f24b9d3b5eba94f667cd272c50c24ecfefc3b4b
SMIX <dj-smix@mail.ru>
2020-11-15 15:03:07 +0300

    Some changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Router } from "react-router-dom"
-import {createBrowserHistory} from 'history'
+import { Router } from "react-router-dom";
+import {createBrowserHistory} from 'history';
 import App from './App.js';
 
 import 'font-awesome5/css/fontawesome-all.css';
 import "animate.css/animate.css";
 
 const history = createBrowserHistory();
-const server = window.location.hostname.length > 7 ? window.location.hostname : "smix-soft.ru";
-const wsport = "8080";
 
 ReactDOM.render(
   <Router history={history}>
